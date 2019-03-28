01a4187e8c5e11411ea2853f6b2f90f8f4e50924
Isa Giongo <isagiongo@gmail.com>
2019-03-28 19:11:20 -0300

    Inicia projeto

---
+++ src/main/java/com/isagiongo/testetutorial/TesteTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.isagiongo.testetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTutorialApplication.class, args);
+	}
+
+}
